b0cf3e03b2b7fd9ca9488d98734825b7a16ab52a
GibranAja <mgibranarraffi@gmail.com>
2024-07-11 19:51:25 +0700

    style: change the scrollbar

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,26 @@
 
 <script>
 export default {
-  name: 'App',
+  name: 'App'
 }
 </script>
+
+<style>
+body {
+  font-family: 'Poppins';
+}
+::-webkit-scrollbar {
+  background: transparent;
+  width: 7px;
+}
+
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #b9b9b9;
+  border-radius: 30px;
+  transition: background 0.3s ease-in-out;
+}
+</style>
