f173605499948f7f1ae16f22742c466f5bdeb0fc
GibranAja <mgibranarraffi@gmail.com>
2024-07-11 07:47:19 +0700

    refactor: remove unused variable

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
-// Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -15,10 +11,4 @@ const vuetify = createVuetify({
   directives,
 })
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(vuetify)
-
-app.mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
