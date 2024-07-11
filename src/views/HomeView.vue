034a3589eaae1997bedfc9e0ee5e1ea63b50eb2a
GibranAja <mgibranarraffi@gmail.com>
2024-07-11 07:00:56 +0700

    feat: implement Pokemon search and display functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>THai Beb</h1>
+  <v-container fluid class="pa-0" style="max-width: 1200px; margin: 0 auto;">
+    <SearchBar @search="searchPokemon"></SearchBar>
+    <PokemonGrid :pokemons="filteredPokemons"></PokemonGrid>
+  </v-container>
 </template>
+
+<script setup>
+import { ref, onMounted, computed } from 'vue';
+import SearchBar from '../components/SearchBar.vue'
+import PokemonGrid from '../components/PokemonGrid.vue'
+
+const pokemons = ref([])
+const searchQuery = ref('')
+
+const fetchPokemons = async() => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=300')
+  const data = await response.json()
+  const pokemonDetails = await Promise.all(
+    data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url)
+      return res.json
+    })
+  )
+  pokemons.value = pokemonDetails
+}
+
+onMounted(fetchPokemons)
+
+const searchPokemon = (query) => {
+  searchQuery.value = query
+}
+
+const filteredPokemons = computed(() => {
+  return pokemons.value.filter(pokemon =>
+    pokemon.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  )
+})
+</script>
