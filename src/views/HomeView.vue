d93b8e04200715efd0672d3ed5265ba095f2658b
GibranAja <mgibranarraffi@gmail.com>
2024-07-11 16:57:09 +0700

    feat: add Load More button functionality to display 40 pokemon at time

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <v-container fluid class="pa-0" style="max-width: 1200px; margin: 0 auto">
     <SearchBar class="d-flex justify-center mt-5" @search="searchPokemon"></SearchBar>
-    <PokemonGrid :pokemons="filteredPokemons"></PokemonGrid>
+    <PokemonGrid :pokemons="visiblePokemons"></PokemonGrid>
+    <div class="d-flex justify-center mt-3">
+      <v-btn @click="loadMorePokemons" v-if="showLoadMore" color="orange" class="text-white">Load More</v-btn>
+    </div>
   </v-container>
 </template>
 
@@ -12,9 +15,10 @@ import PokemonGrid from '../components/PokemonGrid.vue'
 
 const pokemons = ref([])
 const searchQuery = ref('')
+const visiblePokemonsCount = ref(40)
 
 const fetchPokemons = async () => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000')
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1100')
   const data = await response.json()
   pokemons.value = await Promise.all(
     data.results.map(async (pokemon) => {
@@ -28,6 +32,7 @@ onMounted(fetchPokemons)
 
 const searchPokemon = (query) => {
   searchQuery.value = query
+  visiblePokemonsCount.value = 40
 }
 
 const filteredPokemons = computed(() => {
@@ -35,4 +40,16 @@ const filteredPokemons = computed(() => {
     pokemon.name.toLowerCase().includes(searchQuery.value.toLowerCase())
   )
 })
+
+const visiblePokemons = computed(() => {
+  return filteredPokemons.value.slice(0, visiblePokemonsCount.value)
+})
+
+const showLoadMore = computed(() => {
+  return visiblePokemonsCount.value < filteredPokemons.value.length
+})
+
+const loadMorePokemons = () => {
+  visiblePokemonsCount.value += 40
+}
 </script>
