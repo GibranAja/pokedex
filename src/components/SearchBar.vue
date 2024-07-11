d97e7a0bf0c4b72c05a79bc32e3f51411fed31a5
GibranAja <mgibranarraffi@gmail.com>
2024-07-11 07:05:54 +0700

    feat: add SearchBar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-textfield
+    v-model="searchQuery"
+    label="Search Pokemon"
+    @input="$emit('search', 'searchQuery')"
+    prepend-inner-icon="mdi-magnify"
+    class="mt-5"
+    style="max-width: 250px"
+    outlined
+    dense
+    ></v-textfield>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+</script>
