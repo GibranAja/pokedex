4e124b0f90ef3136008f07daddedf90a01a11bf1
GibranAja <mgibranarraffi@gmail.com>
2024-07-11 07:04:56 +0700

    feat: Create grid layout for displaying multiple Pokemon

---
+++ src/components/PokemonGrid.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-col v-for="pokemon in pokemons" :key="pokemon.id" cols="12" sm="4" md="4"></v-col>
+            <PokemonCard :pokemon="pokemon"></PokemonCard>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import PokemonCard from './PokemonCard.vue'
+
+defineProps(['pokemon'])
+</script>
