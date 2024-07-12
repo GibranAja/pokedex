e47079a927cada507281059c0a36be393c375f00
GibranAja <mgibranarraffi@gmail.com>
2024-07-12 18:59:26 +0700

    feat: add footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-footer style="background-color: #fecb05">
+    <v-container fluid>
+      <v-row justify="center" no-gutters>
+        <v-col cols="12" sm="8" md="6" lg="4" class="text-center py-4 text-white">
+          {{ new Date().getFullYear() }} - <strong>Gibran</strong>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
