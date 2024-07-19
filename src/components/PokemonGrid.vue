70b7ea5d6e44c543149ae42249a8717ddb92f1fd
GibranAja <mgibranarraffi@gmail.com>
2024-07-19 19:18:18 +0700

    fix: fixed pokemon id

--- src/components/PokemonGrid.vue
+++ src/components/PokemonGrid.vue
@@ -1,16 +1,16 @@
 <template>
-    <v-container fluid>
-      <v-row>
-        <v-col v-for="pokemon in pokemons" :key="pokemon.id" cols="12" sm="4" md="4">
-          <PokemonCard :pokemon="pokemon" />
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script setup>
-  import { defineProps } from 'vue'
-  import PokemonCard from './PokemonCard.vue'
-  
-  defineProps(['pokemons'])
-  </script>
+  <v-container fluid>
+    <v-row>
+      <v-col v-for="pokemon in pokemons" :key="pokemon.id" cols="12" sm="4" md="4">
+        <PokemonCard :pokemon="pokemon" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import PokemonCard from './PokemonCard.vue'
+
+defineProps(['pokemons'])
+</script>
