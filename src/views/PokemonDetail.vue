da9d3045cade07ae15fb227203999eee9ac83b99
GibranAja <mgibranarraffi@gmail.com>
2024-07-19 19:21:07 +0700

    style: improve code formatting

--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -132,14 +132,14 @@ const getTypeColor = (type) => typeColors[type] || '#A8A878'
 const getStatColor = (stat) => statColors[stat] || '#A8A878'
 
 const getTextColor = (backgroundColor) => {
-  const hex = backgroundColor.replace('#', '');
-  const r = parseInt(hex.substr(0, 2), 16);
-  const g = parseInt(hex.substr(2, 2), 16);
-  const b = parseInt(hex.substr(4, 2), 16);
-  
-  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-  
-  return brightness > 128 ? 'white' : 'black';
+  const hex = backgroundColor.replace('#', '')
+  const r = parseInt(hex.substr(0, 2), 16)
+  const g = parseInt(hex.substr(2, 2), 16)
+  const b = parseInt(hex.substr(4, 2), 16)
+
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000
+
+  return brightness > 128 ? 'white' : 'black'
 }
 
 const capitalizeFirstLetter = (string) => {
